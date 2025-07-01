5823ee07f569862fdf3c756d8c56b707fca6ef57
Vishwajeet Salunkhe <vishwajeetds99@gmail.com>
2025-07-01 12:19:58 +0530

    Backend Structure created

---
+++ online-car-service-station-backend/src/main/java/com/example/online_car_service_station_backend/OnlineCarServiceStationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_car_service_station_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCarServiceStationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCarServiceStationBackendApplication.class, args);
+	}
+
+}
---
+++ online-car-service-station-backend/src/test/java/com/example/online_car_service_station_backend/OnlineCarServiceStationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_car_service_station_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCarServiceStationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
